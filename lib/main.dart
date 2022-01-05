bb3199c858f4daf582e7d16d6afa8e3a70f7f5ec
TH4LY5 <thalyscosta99@gmail.com>
2022-01-05 20:01:59 -0300

    app buscador de gif usando api do giphy.com

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import 'ui/HomePage.dart';
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(highlightColor: Colors.white),
+  ));
+}
+
